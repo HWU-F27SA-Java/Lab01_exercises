5b864a701cd1ddd480eec6ce38a03fb58fde8315
Batatia, Hadj <hb149@hw.ac.uk>
2022-08-03 14:14:25 +0100

    Create HelloWorld.java

---
+++ src/HelloWorld.java
@@ -0,0 +1,17 @@
+/**
+ * This program outputs the phrase Hello World! 
+ */
+public class HelloWorld {
+
+	/**
+	 * 'main' is where execution of the program begins
+	 */
+	public static void main(String[] args) {
+	    
+		// This tells Java to print out the specified string
+		// of text to the system's standard output.
+		// In Eclipse, it will appear in the Console.
+		System.out.println("Hello World!");
+	}
+
+}
